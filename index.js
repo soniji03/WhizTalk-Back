c3e8626d3539d6825e5573224d7bfd59abdd3750
soniji03 <jbpomsoni03@gmail.com>
2024-09-03 17:19:53 +0530

    4545

--- index.js
+++ index.js
@@ -14,14 +14,27 @@ require('./models/ConversationModel');
 // }))
 
 
-const corsOptions = {
-  origin: process.env.FRONTEND_URL || 'https://whiztalk.netlify.app',
-  credentials: true,
-  optionsSuccessStatus: 200, 
-  allowedHeaders: ['Content-Type', 'Authorization']
-};
+// const corsOptions = {
+//   origin: process.env.FRONTEND_URL || 'https://whiztalk.netlify.app',
+//   credentials: true,
+//   optionsSuccessStatus: 200, 
+//   allowedHeaders: ['Content-Type', 'Authorization']
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+
+const allowedOrigins = ['https://whiztalk.netlify.app', 'http://localhost:5173'];
+
+app.use(cors({
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
  
 
