c7a4bce916cba5b5ce8ad9f81aa4ef794d2eef1e
soniji03 <jbpomsoni03@gmail.com>
2024-09-03 17:23:45 +0530

    ty4ui55

--- index.js
+++ index.js
@@ -23,18 +23,16 @@ require('./models/ConversationModel');
 
 // app.use(cors(corsOptions));
 
-const allowedOrigins = ['https://whiztalk.netlify.app', 'http://localhost:5173'];
-
-app.use(cors({
-  origin: function(origin, callback){
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
+const corsOptions = {
+    origin: process.env.FRONTEND_URL,
+    optionsSuccessStatus: 200 // For legacy browser support
+  };
+  app.use(cors({
+    origin: [
+      'https://fullstackdevloperwithom.netlify.app',
+      'http://localhost:5173'
+    ],
+  }));
 
  
 
